c64087f7d3e38fa4ad6e2730a3864e62a5e16d2f
Robert Howden <robert.howden@current-rms.com>
2020-04-20 15:56:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,4 +3,9 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(a, b) {
   a.unshift(b)
 return a
+}
+
+destructivelyAddElementToBeginningOfArray = (a, b) {
+  a.unshift(b)
+return a
 } 
